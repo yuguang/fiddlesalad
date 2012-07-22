fb8a8dd6d8b30e710c75d7abda9e2cf21f3ad1c6
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2012-07-22 11:30:51 -0400

    add google custom search id

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,7 @@
 def site(context):
     return {
         'favicon': 'salad.png',
-        'application': 'Fiddle Salad'
+        'application': 'Fiddle Salad',
+        'searchid': '012088697717623485820:stua8ukdyik'
     }
 
