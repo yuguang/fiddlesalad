991091254045409b37104781547895bd32e9cc32
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2013-08-10 10:39:08 -0400

    add context processor for displaying languages in navigation area

--- context_processors.py
+++ context_processors.py
@@ -2,6 +2,7 @@ def site(context):
     return {
         'favicon': 'salad.png?v=2013071721',
         'application': 'Fiddle Salad',
-        'searchid': '012088697717623485820:stua8ukdyik'
+        'searchid': '012088697717623485820:stua8ukdyik',
+        'multiple_languages': True
     }
 
