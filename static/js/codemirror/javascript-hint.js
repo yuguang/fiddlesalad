51e54e9963c6068038c77a3f50f4cc7303fff035
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2013-11-11 20:48:16 -0500

    no hints for comments or strings

--- static/js/codemirror/javascript-hint.js
+++ static/js/codemirror/javascript-hint.js
@@ -6,6 +6,7 @@
   function scriptHint(editor, keywords, getToken, options) {
     // Find the token at the cursor
     var cur = editor.getCursor(), token = getToken(editor, cur), tprop = token;
+    if (/\b(?:string|comment)\b/.test(token.type)) return;
     token.state = CodeMirror.innerMode(editor.getMode(), token.state).state;
     var str = token.string;
     // if last character is an operator
