a1719de1561921db2b925a8277f4f110a56a0216
yuguang <zh.yuguang.cn@gmail.com>
2015-01-18 09:36:29 -0500

    update fiddlesalad icon

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,6 @@
 def site(context):
     return {
-        'favicon': 'salad.png?v=2013071721',
+        'favicon': 'fiddlesalad.ico',
         'application': 'Fiddle Salad',
         'searchid': '012088697717623485820:stua8ukdyik',
         'multiple_languages': True
